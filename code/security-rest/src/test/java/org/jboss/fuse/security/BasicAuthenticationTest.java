7b7aa7b40fa1d39bab43d58ac7c0dfafd5b930f3
opentlc-user <opentlc-user@forge.opentlc.com>
2016-04-28 09:37:27 -0400

    code skeleton from jenkins

---
+++ code/security-rest/src/test/java/org/jboss/fuse/security/BasicAuthenticationTest.java
@@ -0,0 +1,15 @@
+package org.jboss.fuse.security;
+
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class BasicAuthenticationTest extends CamelTestSupport {
+
+    @Test
+    public void UsernameTest() {
+
+    }
+
+
+
+}
