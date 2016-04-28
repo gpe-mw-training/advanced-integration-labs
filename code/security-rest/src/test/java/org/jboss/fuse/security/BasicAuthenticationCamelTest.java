458ad3317dc4dde174287eec4c9b0ab517b07a0f
opentlc-user <opentlc-user@forge.opentlc.com>
2016-04-28 12:37:28 -0400

    code skeleton from jenkins

---
+++ code/security-rest/src/test/java/org/jboss/fuse/security/BasicAuthenticationCamelTest.java
@@ -0,0 +1,57 @@
+package org.jboss.fuse.security;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.JndiRegistry;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.util.security.Constraint;
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Arrays;
+
+public class BasicAuthenticationCamelTest extends BaseJettyTest {
+
+    @Override
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry jndi = super.createRegistry();
+        jndi.bind("myAuthHandler", getSecurityHandler());
+        return jndi;
+    }
+
+    private SecurityHandler getSecurityHandler() throws IOException {
+        Constraint constraint = new Constraint(Constraint.__BASIC_AUTH, "user");
+        constraint.setAuthenticate(true);
+
+        ConstraintMapping cm = new ConstraintMapping();
+        cm.setPathSpec("/*");
+        cm.setConstraint(constraint);
+
+        ConstraintSecurityHandler sh = new ConstraintSecurityHandler();
+        sh.setAuthenticator(new BasicAuthenticator());
+        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[] {cm}));
+
+        HashLoginService loginService = new HashLoginService("MyRealm", "src/test/resources/org/jboss/fuse/basic/myRealm.properties");
+        sh.setLoginService(loginService);
+        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[]{cm}));
+
+        return sh;
+    }
+
+    @Test
+    public void testHttpBasicAuthCamel() throws Exception {
+        String out = template.requestBody("http://localhost:{{port1}}/test?authMethod=Basic&authUsername=donald&authPassword=duck", "Hello World", String.class);
+        assertEquals("Bye World", out);
+    }
+
+
+
+
+}
---
+++ code/security-rest/src/test/java/org/jboss/fuse/security/BasicAuthenticationRESTCamelDSLTest.java
@@ -0,0 +1,121 @@
+package org.jboss.fuse.security;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.HttpOperationFailedException;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestPropertyDefinition;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.util.security.Constraint;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BasicAuthenticationRESTCamelDSLTest extends BaseJettyTest {
+
+    private static String HOST = "localhost";
+    private static int PORT = getPort1();
+
+    @Override
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry jndi = super.createRegistry();
+        jndi.bind("myAuthHandler", getSecurityHandler());
+        return jndi;
+    }
+
+    private SecurityHandler getSecurityHandler() throws IOException {
+        Constraint constraint = new Constraint(Constraint.__BASIC_AUTH, "user");
+        constraint.setAuthenticate(true);
+
+        ConstraintMapping cm = new ConstraintMapping();
+        cm.setPathSpec("/*");
+        cm.setConstraint(constraint);
+
+        ConstraintSecurityHandler sh = new ConstraintSecurityHandler();
+        sh.setAuthenticator(new BasicAuthenticator());
+        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[] {cm}));
+
+        HashLoginService loginService = new HashLoginService("MyRealm", "src/test/resources/org/jboss/fuse/basic/myRealm.properties");
+        sh.setLoginService(loginService);
+        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[]{cm}));
+
+        return sh;
+    }
+
+
+
+    protected HttpResponse runAndValidate(String host, String url, String user, String password, String realm) {
+
+        HttpResponse response = new HttpResponse();
+
+        // Define the Get Method with the String of the url to access the HTTP Resource
+        GetMethod get = new GetMethod(url);
+
+        // Set Credentials
+        Credentials creds = new UsernamePasswordCredentials(user,password);
+        // Auth Scope
+        AuthScope authScope = new AuthScope(host,PORT,realm);
+
+        // Execute request
+        try {
+            // Get HTTP client
+            HttpClient httpclient = new HttpClient();
+            // Use preemptive to select BASIC Auth
+            httpclient.getParams().setAuthenticationPreemptive(true);
+            httpclient.getState().setCredentials(authScope,creds);
+            response.setStatus(httpclient.executeMethod(get));
+
+            InputStream is = get.getResponseBodyAsStream();
+            Scanner s = new Scanner(is).useDelimiter("\\A");
+            response.setMessage(s.hasNext() ? s.next() : "");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Release current connection to the connection pool once you are done
+            get.releaseConnection();
+        }
+
+        return response;
+    }
+
+
+    protected class HttpResponse {
+        int status;
+        String message;
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
+
+
+
+}
