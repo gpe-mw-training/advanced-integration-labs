e58e52273718eccc01a5f44d249977573adc371b
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-04 03:52:30 -0400

    code skeleton from jenkins

--- code/security-rest/src/test/java/org/jboss/fuse/security/camel/policy/SimpleAutPolicyTest.java
+++ code/security-rest/src/test/java/org/jboss/fuse/security/camel/policy/SimpleAutPolicyTest.java
@@ -27,9 +27,9 @@ public class SimpleAutPolicyTest extends BaseNetty4Test {
             template.requestBodyAndHeader("netty4-http://http://localhost:" + PORT + "/say/hello/noauthheader", "", Exchange.HTTP_METHOD,"GET",String.class);
             fail("Should send back 500");
         } catch (CamelExecutionException e) {
-            NettyHttpOperationFailedException cause = assertIsInstanceOf(
-                    NettyHttpOperationFailedException.class, e.getCause());
+            NettyHttpOperationFailedException cause = assertIsInstanceOf(NettyHttpOperationFailedException.class, e.getCause());
             assertEquals(500, cause.getStatusCode());
+            assertEquals("",cause.getMessage());
         }
 
         // Authorized with username:password is mickey:mouse
