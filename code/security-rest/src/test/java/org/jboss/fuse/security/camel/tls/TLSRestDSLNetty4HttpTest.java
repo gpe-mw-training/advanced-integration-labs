958e6822a28e45de08f197ef70b255f27e321b7f
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-12 11:42:30 -0400

    code skeleton from jenkins

--- code/security-rest/src/test/java/org/jboss/fuse/security/camel/tls/TLSRestDSLNetty4HttpTest.java
+++ code/security-rest/src/test/java/org/jboss/fuse/security/camel/tls/TLSRestDSLNetty4HttpTest.java
@@ -21,10 +21,9 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
         JndiRegistry jndi = super.createRegistry();
 
         // Netty with HTTPS scheme, JAAS Auth & Security Path Constraint & Role
-        jndi.bind("nettyServerConfiguration", getNettyHttpSslConfiguration());
+        jndi.bind("sslServerParameters",getSSLContextParameters());
         jndi.bind("nettyServerSecurityConfig", getJAASSecurityHttpConfiguration());
 
-        // jndi.bind("nettyClientConf", getNettyHttpClientSslConfiguration()); // DOESN't WORK for the producer
         // Pass to the Netty Producer the SSL Context Parameters
         jndi.bind("sslClientParameters", getClientSSLContextParameters());
         return jndi;
@@ -37,9 +36,6 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
         // and the object is not recreated with the System Prop
         URL jaasURL =  this.getClass().getResource("/org/jboss/fuse/security/basic/myrealm-jaas.cfg");
         setSystemProp("java.security.auth.login.config", jaasURL.toExternalForm());
-
-        URL trustStoreUrl = this.getClass().getResource("clientKeystore.jks");
-        setSystemProp("javax.net.ssl.trustStore", trustStoreUrl.toURI().getPath());
 
         //setSystemProp("javax.net.debug","all");
         super.setUp();
@@ -54,7 +50,7 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
     @Test
     public void testFailAuth() {
         try {
-            template.requestBody("netty4-http://https://localhost:" + PORT + "/say/hello/noauthheader","", String.class);
+            template.requestBody("netty4-http://https://localhost:" + PORT + "/say/hello/noauthheader?ssl=true&sslContextParameters=#sslClientParameters","", String.class);
             fail("Should send back 401");
         } catch (CamelExecutionException e) {
             NettyHttpOperationFailedException cause = assertIsInstanceOf(NettyHttpOperationFailedException.class, e.getCause());
@@ -64,7 +60,6 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
 
     @Test
     public void testBasicAuth()  {
-
         // username:password is mickey:mouse
         String auth = "Basic bWlja2V5Om1vdXNl";
         String result = template.requestBodyAndHeader("netty4-http://https://localhost:" + PORT + "/say/hello/Mickey?ssl=true&sslContextParameters=#sslClientParameters", "", "Authorization", auth, String.class);
@@ -81,9 +76,8 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
             String result = template.requestBodyAndHeader("netty4-http://https://localhost:" + PORT + "/say/hello/Donald?ssl=true&sslContextParameters=#sslClientParameters", "", "Authorization", auth, String.class);
             fail("Should send back 401");
         } catch (CamelExecutionException e) {
-            //NettyHttpOperationFailedException cause = assertIsInstanceOf(NettyHttpOperationFailedException.class, e.getCause());
-            //assertEquals(401, cause.getStatusCode());
-            e.printStackTrace();
+            NettyHttpOperationFailedException cause = assertIsInstanceOf(NettyHttpOperationFailedException.class, e.getCause());
+            assertEquals(401, cause.getStatusCode());
         }
     }
 
@@ -98,6 +92,20 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
     }
 
 
+    private SSLContextParameters getClientSSLContextParameters() {
+        // TLS
+        KeyStoreParameters ksp = new KeyStoreParameters();
+        ksp.setResource("org/jboss/fuse/security/camel/tls/clientKeystore.jks");
+        ksp.setPassword("cspass");
+
+        TrustManagersParameters tmp = new TrustManagersParameters();
+        tmp.setKeyStore(ksp);
+
+        SSLContextParameters scp = new SSLContextParameters();
+        scp.setTrustManagers(tmp);
+        return scp;
+    }
+
     /*
      * NettyHttpConfiguration with SSL parameters
      */
@@ -110,24 +118,14 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
         return conf;
     }
 
-    private SSLContextParameters getClientSSLContextParameters() {
-        // TLS
-        KeyStoreParameters ksp = new KeyStoreParameters();
-        ksp.setResource("org/jboss/fuse/security/camel/tls/clientKeystore.jks");
-        ksp.setPassword("cspass");
-
-        TrustManagersParameters tmp = new TrustManagersParameters();
-        tmp.setKeyStore(ksp);
-
-        //KeyManagersParameters kmp = new KeyManagersParameters();
-        //kmp.setKeyStore(ksp);
-        //kmp.setKeyPassword("ckpass");
-
-        SSLContextParameters scp = new SSLContextParameters();
-        //scp.setKeyManagers(kmp);
-        scp.setTrustManagers(tmp);
-        scp.setSecureSocketProtocol("SSLv3");
-        return scp;
+    /*
+     * NettyHttpConfiguration with SSL parameters
+     */
+    private NettyHttpConfiguration getNettyHttpSslConfiguration() {
+        NettyHttpConfiguration conf = new NettyHttpConfiguration();
+        conf.setSslContextParameters(getSSLContextParameters());
+        return conf;
     }
+
 
 }
