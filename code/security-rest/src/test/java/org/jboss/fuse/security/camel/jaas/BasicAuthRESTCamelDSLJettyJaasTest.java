8de343a539f2554527bf5834f0c21fdf92cb7eaf
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-13 08:42:27 -0400

    code skeleton from jenkins

--- code/security-rest/src/test/java/org/jboss/fuse/security/camel/jaas/BasicAuthRESTCamelDSLJettyJaasTest.java
+++ code/security-rest/src/test/java/org/jboss/fuse/security/camel/jaas/BasicAuthRESTCamelDSLJettyJaasTest.java
@@ -36,14 +36,10 @@ public class BasicAuthRESTCamelDSLJettyJaasTest extends BaseJettyTest {
     @Override
     protected JndiRegistry createRegistry() throws Exception {
         JndiRegistry jndi = super.createRegistry();
-        jndi.bind("myAuthHandler", getSecurityHandler());
-        return jndi;
     }
 
     @Override
     public void setUp() throws Exception {
-        URL jaasURL =  this.getClass().getResource("/org/jboss/fuse/security/basic/myrealm-jaas.cfg");
-        setSystemProp("java.security.auth.login.config", jaasURL.toExternalForm());
         super.setUp();
     }
 
@@ -53,7 +49,11 @@ public class BasicAuthRESTCamelDSLJettyJaasTest extends BaseJettyTest {
         super.tearDown();
     }
 
+    }
 
+    @Test
+    public void UsernameWrongPasswordTest() {
+    }
 
 
     private HttpResult runAndValidate(String host, String url, String user, String password, String realm) {
@@ -91,47 +91,6 @@ public class BasicAuthRESTCamelDSLJettyJaasTest extends BaseJettyTest {
     }
 
     private SecurityHandler getSecurityHandler() throws IOException {
-        // Describe the Authentication Constraint to be applied (BASIC, DISGEST, NEGOTIATE, ...)
-        Constraint constraint = new Constraint(Constraint.__BASIC_AUTH, "user");
-        constraint.setAuthenticate(true);
-
-        // Define Constraint mapping
-        ConstraintMapping cm = new ConstraintMapping();
-        cm.setPathSpec("/*");
-        cm.setConstraint(constraint);
-
-        /* A security handler is a jetty handler that secures content behind a
-         *  particular portion of a url space. The ConstraintSecurityHandler is a
-         *  more specialized handler that allows matching of urls to different
-         *  constraints. The server sets this as the first handler in the chain,
-         *  effectively applying these constraints to all subsequent handlers in
-         *  the chain.
-         *  The BasicAuthenticator instance is the object that actually checks the credentials
-         */
-        ConstraintSecurityHandler sh = new ConstraintSecurityHandler();
-        sh.setAuthenticator(new BasicAuthenticator());
-        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[] { cm }));
-
-        /*
-         * The DefaultIdentityService service handles only role reference maps passed in an
-         * associated org.eclipse.jetty.server.UserIdentity.Scope.  If there are roles
-         * refs present, then associate will wrap the UserIdentity with one that uses the role references in the
-         * org.eclipse.jetty.server.UserIdentity#isUserInRole(String, org.eclipse.jetty.server.UserIdentity.Scope)}
-         * implementation.
-         *
-        */
-        DefaultIdentityService dis = new DefaultIdentityService();
-
-        // Service which create a UserRealm suitable for use with JAAS
-        JAASLoginService loginService = new JAASLoginService();
-        loginService.setName("myrealm");
-        loginService.setLoginModuleName("propsFileModule");
-        loginService.setIdentityService(dis);
-
-        sh.setLoginService(loginService);
-        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[] { cm }));
-
-        return sh;
     }
 
 
