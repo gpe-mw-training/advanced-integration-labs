04eece0fc382b1643d86a3be0f4a1185daa7afe6
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-13 04:52:27 -0400

    code skeleton from jenkins

--- code/security-rest/src/test/java/org/jboss/fuse/security/camel/basic/BasicAuthRESTCamelDSLJettyHashLoginTest.java
+++ code/security-rest/src/test/java/org/jboss/fuse/security/camel/basic/BasicAuthRESTCamelDSLJettyHashLoginTest.java
@@ -33,42 +33,18 @@ public class BasicAuthRESTCamelDSLJettyHashLoginTest extends BaseJettyTest {
     @Override
     protected JndiRegistry createRegistry() throws Exception {
         JndiRegistry jndi = super.createRegistry();
-        jndi.bind("myAuthHandler", getSecurityHandler());
-        return jndi;
     }
 
     private SecurityHandler getSecurityHandler() throws IOException {
-        // Describe the Authentication Constraint to be applied (BASIC, DISGEST, NEGOTIATE, ...)
-        Constraint constraint = new Constraint(Constraint.__BASIC_AUTH, "user");
-        constraint.setAuthenticate(true);
-
-        // Map the Auth Contrainst with a Path
-        ConstraintMapping cm = new ConstraintMapping();
-        cm.setPathSpec("/*");
-        cm.setConstraint(constraint);
-
-        /* A security handler is a jetty handler that secures content behind a
-           particular portion of a url space. The ConstraintSecurityHandler is a
-           more specialized handler that allows matching of urls to different
-           constraints. The server sets this as the first handler in the chain,
-           effectively applying these constraints to all subsequent handlers in
-           the chain.
-           The BasicAuthenticator instance is the object that actually checks the credentials
-        */
-        ConstraintSecurityHandler sh = new ConstraintSecurityHandler();
-        sh.setAuthenticator(new BasicAuthenticator());
-        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[] { cm }));
-
-        //  The HashLogin is an implementation of a UserRealm that stores users and roles in-memory in HashMaps.
-        HashLoginService loginService = new HashLoginService("MyRealm",
-                "src/test/resources/org/jboss/fuse/security/basic/myrealm.props");
-        sh.setLoginService(loginService);
-        sh.setConstraintMappings(Arrays.asList(new ConstraintMapping[] { cm }));
-
-        return sh;
     }
 
+    @Test
+    public void UsernameTest() {
+     }
 
+    @Test
+    public void UsernameWrongPasswordTest() {
+    }
 
 
 
