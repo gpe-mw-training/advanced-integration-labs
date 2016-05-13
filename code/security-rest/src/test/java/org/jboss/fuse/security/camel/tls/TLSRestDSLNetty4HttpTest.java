0984d8e395f85aa5dc252da3a8f9ffcdf5bb6542
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-13 04:12:27 -0400

    code skeleton from jenkins

--- code/security-rest/src/test/java/org/jboss/fuse/security/camel/tls/TLSRestDSLNetty4HttpTest.java
+++ code/security-rest/src/test/java/org/jboss/fuse/security/camel/tls/TLSRestDSLNetty4HttpTest.java
@@ -20,13 +20,8 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
     protected JndiRegistry createRegistry() throws Exception {
         JndiRegistry jndi = super.createRegistry();
 
-        // Netty with HTTPS scheme, JAAS Auth & Security Path Constraint & Role
-        jndi.bind("sslServerParameters",getSSLContextParameters());
-        jndi.bind("nettyServerSecurityConfig", getJAASSecurityHttpConfiguration());
-
-        // Pass to the Netty Producer the SSL Context Parameters
-        jndi.bind("sslClientParameters", getClientSSLContextParameters());
         return jndi;
+
     }
 
     @Override
@@ -49,83 +44,17 @@ public class TLSRestDSLNetty4HttpTest extends BaseNetty4Test {
 
     @Test
     public void testFailAuth() {
-        try {
-            template.requestBody("netty4-http://https://localhost:" + PORT + "/say/hello/noauthheader?ssl=true&sslContextParameters=#sslClientParameters","", String.class);
-            fail("Should send back 401");
-        } catch (CamelExecutionException e) {
-            NettyHttpOperationFailedException cause = assertIsInstanceOf(NettyHttpOperationFailedException.class, e.getCause());
-            assertEquals(401, cause.getStatusCode());
-        }
     }
 
     @Test
     public void testBasicAuth()  {
-        // username:password is mickey:mouse
-        String auth = "Basic bWlja2V5Om1vdXNl";
-        String result = template.requestBodyAndHeader("netty4-http://https://localhost:" + PORT + "/say/hello/Mickey?ssl=true&sslContextParameters=#sslClientParameters", "", "Authorization", auth, String.class);
-        assertEquals("\"Hello World Mickey\"", result);
     }
 
     @Test
     public void testBasicAuthSecConstraintWithoutAdminRole() {
-        // username:password is donald:duck
-        String auth = "Basic ZG9uYWxkOmR1Y2s=";
-
-        // User without Admin Role
-        try {
-            String result = template.requestBodyAndHeader("netty4-http://https://localhost:" + PORT + "/say/hello/Donald?ssl=true&sslContextParameters=#sslClientParameters", "", "Authorization", auth, String.class);
-            fail("Should send back 401");
-        } catch (CamelExecutionException e) {
-            NettyHttpOperationFailedException cause = assertIsInstanceOf(NettyHttpOperationFailedException.class, e.getCause());
-            assertEquals(401, cause.getStatusCode());
-        }
-    }
-
-    @Test
-    public void testBasicAuthAndSecConstraint() {
-        // username:password is mickey:mouse
-        String auth = "Basic bWlja2V5Om1vdXNl";
-
-        // User with Role Admin
-        String result = template.requestBodyAndHeader("netty4-http://https://localhost:" + PORT + "/say/hello/Mickey?ssl=true&sslContextParameters=#sslClientParameters", "", "Authorization", auth, String.class);
-        assertEquals("\"Hello World Mickey\"", result);
     }
 
 
-    private SSLContextParameters getClientSSLContextParameters() {
-        // TLS
-        KeyStoreParameters ksp = new KeyStoreParameters();
-        ksp.setResource("org/jboss/fuse/security/camel/tls/clientKeystore.jks");
-        ksp.setPassword("cspass");
-
-        TrustManagersParameters tmp = new TrustManagersParameters();
-        tmp.setKeyStore(ksp);
-
-        SSLContextParameters scp = new SSLContextParameters();
-        scp.setTrustManagers(tmp);
-        return scp;
-    }
-
-    /*
-     * NettyHttpConfiguration with SSL parameters
-     */
-    private NettyHttpConfiguration getNettyHttpClientSslConfiguration() {
-        NettyHttpConfiguration conf = new NettyHttpConfiguration();
-        conf.setSsl(true);
-        //conf.setKeyStoreResource("org/jboss/fuse/security/camel/tls/clientKeystore.jks");
-        //conf.setTrustStoreResource("org/jboss/fuse/security/camel/tls/clientKeystore.jks");
-        conf.setSslContextParameters(getClientSSLContextParameters());
-        return conf;
-    }
-
-    /*
-     * NettyHttpConfiguration with SSL parameters
-     */
-    private NettyHttpConfiguration getNettyHttpSslConfiguration() {
-        NettyHttpConfiguration conf = new NettyHttpConfiguration();
-        conf.setSslContextParameters(getSSLContextParameters());
-        return conf;
-    }
 
 
 }
