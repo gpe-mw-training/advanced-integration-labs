066fc8bea9f31f02158951ef2b9c04d78901a50f
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-24 12:47:25 -0400

    code skeleton from jenkins

--- code/persistence/src/test/java/org/jboss/fuse/persistence/AbstractJdbcTestSupport.java
+++ code/persistence/src/test/java/org/jboss/fuse/persistence/AbstractJdbcTestSupport.java
@@ -1,14 +1,10 @@
 package org.jboss.fuse.persistence;
 
 import org.apache.camel.impl.JndiRegistry;
-import org.apache.camel.spi.Registry;
-import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.After;
 import org.junit.Before;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 /**
  * Unit test based on user forum request about this component
