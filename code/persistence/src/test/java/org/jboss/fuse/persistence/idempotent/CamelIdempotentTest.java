6700885f21fb5cf5e09ce6120e7a72da581e2415
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-26 15:07:27 -0400

    code skeleton from jenkins

--- code/persistence/src/test/java/org/jboss/fuse/persistence/idempotent/CamelIdempotentTest.java
+++ code/persistence/src/test/java/org/jboss/fuse/persistence/idempotent/CamelIdempotentTest.java
@@ -4,9 +4,14 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.apache.commons.net.ntp.TimeStamp;
+import org.h2.tools.Server;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.*;
 
 public class CamelIdempotentTest extends CamelSpringTestSupport {
 
@@ -21,6 +26,45 @@ public class CamelIdempotentTest extends CamelSpringTestSupport {
         mockResult.expectedMessageCount(2);
         template.requestBodyAndHeader("111,22-04-2016,Claus,Ibsen,incident camel-111,this is a report incident for camel-111,cibsen@gmail.com,+111 10 20 300","CamelRecord",1);
         template.requestBodyAndHeader("222,18-05-2016,Claus,Ibsen,incident camel-222,this is a report incident for camel-222,cibsen@gmail.com,+111 10 20 300","CamelRecord",2);
+        mockResult.assertIsSatisfied();
+    }
+
+    @Test
+    public void testSendSomeCsvRecordWithSameHeader() throws InterruptedException {
+        mockResult.expectedMessageCount(2);
+        template.requestBodyAndHeader("111,22-04-2016,Claus,Ibsen,incident camel-111,this is a report incident for camel-111,cibsen@gmail.com,+111 10 20 300","CamelRecord",1);
+        template.requestBodyAndHeader("222,18-05-2016,Claus,Ibsen,incident camel-222,this is a report incident for camel-222,cibsen@gmail.com,+111 10 20 300","CamelRecord",2);
+        template.requestBodyAndHeader("333,18-05-2016,Claus,Ibsen,incident camel-333,this is a report incident for camel-333,cibsen@gmail.com,+111 10 20 300","CamelRecord",1);
+        mockResult.assertIsSatisfied();
+    }
+
+    @Test
+    public void testStopStartCamelRoute() throws Exception {
+        mockResult.expectedMessageCount(1);
+
+        template.requestBodyAndHeader("111,22-04-2016,Claus,Ibsen,incident camel-111,this is a report incident for camel-111,cibsen@gmail.com,+111 10 20 300","CamelRecord",1);
+        stopCamelContext();
+
+        Connection conn = null;
+        try {
+            Class.forName("org.h2.Driver");
+            conn = DriverManager.getConnection("jdbc:h2:mem:idempotentReport");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from CAMEL_MESSAGEPROCESSED");
+            while (rs.next()) {
+                Assert.assertEquals("1",rs.getString("messageId"));
+                Assert.assertEquals("DirectConsumer",rs.getString("processorName"));
+            }
+        } catch(Exception e) {
+            System.out.print("Something happened");
+        } finally {
+            conn.close();
+        }
+
+        startCamelContext();
+        template.start();
+        template.requestBodyAndHeader("333,18-05-2016,Claus,Ibsen,incident camel-333,this is a report incident for camel-333,cibsen@gmail.com,+111 10 20 300","CamelRecord",1);
+
         mockResult.assertIsSatisfied();
     }
 
