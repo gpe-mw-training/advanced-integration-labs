6ea49e25b57e1cb6ce9cff4a4f3545f51ee3ab70
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-27 10:42:26 -0400

    code skeleton from jenkins

--- code/transaction/src/test/java/org/jboss/fuse/transaction/route/JTATxRouteTest.java
+++ code/transaction/src/test/java/org/jboss/fuse/transaction/route/JTATxRouteTest.java
@@ -9,6 +9,7 @@ import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -25,17 +26,45 @@ public class JTATxRouteTest extends CamelSpringTestSupport {
     }
 
     @Test
-    public void testProducer() throws Exception {
+    public void testInsertRecord() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMessageCount(1);
 
+        // Send a CSV Record to insert it within the DB
         template.sendBody("direct:data-insert", "111,22-04-2016,Claus,Ibsen,incident camel-111,this is a report incident for camel-111,cibsen@gmail.com,+111 10 20 300");
-        template.sendBody("direct:select",null);
 
+        // Perform a Select query to find the record
+        template.sendBody("direct:select",null);
         Exchange exchange = mock.getExchanges().get(0);
+
         assertMockEndpointsSatisfied();
         assertEquals("1, Claus, Ibsen, cibsen@gmail.com, +111 10 20 300, 111, this is a report incident for camel-111, incident camel-111",exchange.getIn().getBody());
-        // assertEquals("Should have deleted record for id 1", new Integer(2), jdbcTemplate.queryForObject("select count(*) from projects", Integer.class));
+
+    }
+
+    @Test
+    public void testRollbackRecord() throws Exception {
+        MockEndpoint mockError = getMockEndpoint("mock:error");
+        mockError.expectedMessageCount(1);
+
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+
+        // Send a CSV Record to insert it within the DB
+        try {
+            template.sendBody("direct:data-insert-rb",
+                    "111,22-04-2016,Claus,Ibsen,incident camel-111,this is a report incident for camel-111,cibsen@gmail.com,+111 10 20 300");
+            fail("Exception expected");
+        } catch(Exception e) {
+            // Record should be rollbacked
+        }
+        // Perform a Select query to find the record
+        template.sendBody("direct:select",null);
+
+        // We expect an exchange with an empty string (= no record found) from the direct-select route
+        Assert.assertEquals(true,((String)mock.getExchanges().get(0).getIn().getBody()).isEmpty());
+
+        assertMockEndpointsSatisfied();
     }
 
 }
