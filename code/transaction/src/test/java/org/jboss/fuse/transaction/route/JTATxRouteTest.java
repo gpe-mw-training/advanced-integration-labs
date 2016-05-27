052cb1c97ae454137b0aa816af88c4501f9c86ca
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-27 11:17:25 -0400

    code skeleton from jenkins

--- code/transaction/src/test/java/org/jboss/fuse/transaction/route/JTATxRouteTest.java
+++ code/transaction/src/test/java/org/jboss/fuse/transaction/route/JTATxRouteTest.java
@@ -10,6 +10,7 @@ import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -25,6 +26,11 @@ public class JTATxRouteTest extends CamelSpringTestSupport {
         return new ClassPathXmlApplicationContext("org/jboss/fuse/transaction/camelContext.xml");
     }
 
+    @Before
+    public void cfgSystemProperties() {
+        System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES","*");
+    }
+
     @Test
     public void testInsertRecord() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
@@ -36,10 +42,17 @@ public class JTATxRouteTest extends CamelSpringTestSupport {
         // Perform a Select query to find the record
         template.sendBody("direct:select",null);
         Exchange exchange = mock.getExchanges().get(0);
-
-        assertMockEndpointsSatisfied();
         assertEquals("1, Claus, Ibsen, cibsen@gmail.com, +111 10 20 300, 111, this is a report incident for camel-111, incident camel-111",exchange.getIn().getBody());
 
+        Thread.sleep(1000);
+
+        // We will check that we have a message within the queue
+        MockEndpoint mockQueue = getMockEndpoint("mock:result-queue");
+        mockQueue.expectedMessageCount(1);
+        String csvRecord = (String)mockQueue.getExchanges().get(0).getIn().getBody();
+        assertEquals("111,22-04-2016,Claus,Ibsen,incident camel-111,this is a report incident for camel-111,cibsen@gmail.com,+111 10 20 300",csvRecord.replaceAll("\\r|\\n", ""));
+
+        assertMockEndpointsSatisfied();
     }
 
     @Test
