3ab8d51a258ad2732be359e303fee284f7fea4ec
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-31 02:57:29 -0400

    code skeleton from jenkins

--- code/persistence/src/test/java/org/jboss/fuse/persistence/jpa/JPACamelWildflyTest.java
+++ code/persistence/src/test/java/org/jboss/fuse/persistence/jpa/JPACamelWildflyTest.java
@@ -11,7 +11,9 @@ import org.jboss.fuse.persistence.jpa.routing.DirectToJPABuilder;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.extension.camel.CamelAware;
@@ -42,24 +44,45 @@ public class JPACamelWildflyTest {
         JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-jpa-test.jar");
         archive.addClass(Account.class);
         archive.addPackage(DirectToJPABuilder.class.getPackage());
+        // Location of the Datasource definition file
+        archive.addAsManifestResource("org/jboss/fuse/persistence/jpa/jbossas-ds.xml","jbossas-ds.xml");
         archive.addAsManifestResource("org/jboss/fuse/persistence/jpa/persistence-jpa.xml", "persistence.xml");
-        archive.addAsManifestResource("org/jboss/fuse/persistence/jpa/jbossas-ds.xml");
-        // Turn our project into CDI
+        // Turn on our project into a CDI IoC one
         archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
         return archive;
     }
 
+    @Before
+    public void setUp() throws Exception {
+        // Insert some test data
+        utx.begin();
+        em.joinTransaction();
+        em.persist(new Account(1, 750));
+        em.persist(new Account(2, 300));
+        utx.commit();
+        em.clear();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Clean up test data
+        utx.begin();
+        em.joinTransaction();
+        em.createQuery("delete from Account").executeUpdate();
+        utx.commit();
+    }
+
     @Test
-    public void testJpaCamelRoute() throws Exception {
+    public void testJpaInsertCamelRoute() throws Exception {
 
         CamelContext camelctx = contextRegistry.getCamelContext("route-cdi-context");
         Assert.assertNotNull("Expected Route CDI context to not be null", camelctx);
 
         // Persist a new account entity
-        Account account = new Account(1, 500);
+        Account account = new Account(3, 800);
         camelctx.createProducerTemplate().sendBody("direct:start", account);
 
-        Account result = em.getReference(Account.class, 1);
+        Account result = em.getReference(Account.class, 3);
 
         Assert.assertEquals(account, result);
     }
