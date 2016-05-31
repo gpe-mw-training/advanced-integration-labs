79226232e9507bed19f96cfac343904cd09c46cf
opentlc-user <opentlc-user@forge.opentlc.com>
2016-05-31 10:22:31 -0400

    code skeleton from jenkins

--- code/persistence/src/test/java/org/jboss/fuse/persistence/jpa/JPACamelWildflyTest.java
+++ code/persistence/src/test/java/org/jboss/fuse/persistence/jpa/JPACamelWildflyTest.java
@@ -27,7 +27,6 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 
 @RunWith(Arquillian.class)
-@CamelAware
 public class JPACamelWildflyTest {
 
     @ArquillianResource
@@ -41,50 +40,19 @@ public class JPACamelWildflyTest {
 
     @Deployment
     public static JavaArchive deployment() {
-        JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "camel-jpa-test.jar");
-        archive.addClass(Account.class);
-        archive.addPackage(DirectToJPABuilder.class.getPackage());
-        // Location of the Datasource definition file
-        archive.addAsManifestResource("org/jboss/fuse/persistence/jpa/jbossas-ds.xml","jbossas-ds.xml");
-        archive.addAsManifestResource("org/jboss/fuse/persistence/jpa/persistence-jpa.xml", "persistence.xml");
-        // Turn on our project into a CDI IoC one
-        archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
         return archive;
     }
 
     @Before
     public void setUp() throws Exception {
-        // Insert some test data
-        utx.begin();
-        em.joinTransaction();
-        em.persist(new Account(1, 750));
-        em.persist(new Account(2, 300));
-        utx.commit();
-        em.clear();
     }
 
     @After
     public void tearDown() throws Exception {
-        // Clean up test data
-        utx.begin();
-        em.joinTransaction();
-        em.createQuery("delete from Account").executeUpdate();
-        utx.commit();
     }
 
     @Test
     public void testJpaInsertCamelRoute() throws Exception {
-
-        CamelContext camelctx = contextRegistry.getCamelContext("route-cdi-context");
-        Assert.assertNotNull("Expected Route CDI context to not be null", camelctx);
-
-        // Persist a new account entity
-        Account account = new Account(3, 800);
-        camelctx.createProducerTemplate().sendBody("direct:start", account);
-
-        Account result = em.getReference(Account.class, 3);
-
-        Assert.assertEquals(account, result);
     }
 
 }
