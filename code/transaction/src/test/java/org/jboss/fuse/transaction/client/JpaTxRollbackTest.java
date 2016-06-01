b314225577c201025353be064846db291de9d74c
opentlc-user <opentlc-user@forge.opentlc.com>
2016-06-01 13:57:31 -0400

    code skeleton from jenkins

--- code/transaction/src/test/java/org/jboss/fuse/transaction/client/JpaTxRollbackTest.java
+++ code/transaction/src/test/java/org/jboss/fuse/transaction/client/JpaTxRollbackTest.java
@@ -1,73 +1,48 @@
 package org.jboss.fuse.transaction.client;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jpa.JpaComponent;
-import org.apache.camel.component.jpa.JpaEndpoint;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.spring.SpringCamelContext;
 import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.camel.test.spring.CamelSpringTestSupport;
-import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
 import org.jboss.fuse.transaction.model.Project;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JpaTxRollbackTest extends CamelTestSupport {
 
     protected ApplicationContext applicationContext;
     protected TransactionTemplate transactionTemplate;
     protected EntityManager entityManager;
+    private static AtomicInteger amq = new AtomicInteger();
+    private static AtomicInteger linux = new AtomicInteger();
+    private static AtomicInteger kaboom = new AtomicInteger();
 
     protected static final String SELECT_ALL_STRING = "select x from " + Project.class.getName() + " x";
 
-/*
-    @Override
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry reg = super.createRegistry();
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("project");
-        // reg.bind("emf",emf);
-
-        JpaTransactionManager jpaTx = new JpaTransactionManager(emf);
-        jpaTx.afterPropertiesSet();
-
-        TransactionTemplate tt = new TransactionTemplate();
-        tt.setTransactionManager(jpaTx);
-
-        JpaComponent comp = new JpaComponent();
-        comp.setTransactionManager(jpaTx);
-        reg.bind("jpa",comp);
-        return reg;
-    }
-*/
-
-    @Before
-    public void setUp() throws Exception {
+    @Before public void setUp() throws Exception {
         super.setUp();
-        EntityManagerFactory entityManagerFactory = applicationContext.getBean("entityManagerFactory",
-                EntityManagerFactory.class);
+        EntityManagerFactory entityManagerFactory = applicationContext
+                .getBean("entityManagerFactory", EntityManagerFactory.class);
         transactionTemplate = applicationContext.getBean("transactionTemplate", TransactionTemplate.class);
         entityManager = entityManagerFactory.createEntityManager();
         cleanupRepository();
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @After public void tearDown() throws Exception {
         super.tearDown();
         if (entityManager != null) {
             entityManager.close();
@@ -88,20 +63,18 @@ public class JpaTxRollbackTest extends CamelTestSupport {
         });
     }
 
-    @Override
-    protected CamelContext createCamelContext() throws Exception {
-        applicationContext = new org.springframework.context.support.ClassPathXmlApplicationContext("org/jboss/fuse/transaction/springJpaRouteTest.xml");
+    @Override protected CamelContext createCamelContext() throws Exception {
+        applicationContext = new org.springframework.context.support.ClassPathXmlApplicationContext(
+                "org/jboss/fuse/transaction/springJpaRouteTest.xml");
         return SpringCamelContext.springCamelContext(applicationContext);
     }
 
-    @Test
-    public void testDelete() throws Exception {
+    @Test public void testDelete() throws Exception {
     }
 
     protected String getEndpointUri() {
         return "jpa://" + Project.class.getName() + "?consumer.transacted=true&delay=1000";
     }
-
 
     protected void assertEntityInDB(int size) throws Exception {
         List<?> list = entityManager.createQuery(selectAllString()).getResultList();
