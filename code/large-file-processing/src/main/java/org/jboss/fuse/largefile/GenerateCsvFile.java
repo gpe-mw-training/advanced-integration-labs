0ae3b80a79604ca3c05f8c71b71c6cca00576292
opentlc-user <opentlc-user@forge.opentlc.com>
2016-07-15 12:52:39 -0400

    code skeleton from jenkins

---
+++ code/large-file-processing/src/main/java/org/jboss/fuse/largefile/GenerateCsvFile.java
@@ -0,0 +1,100 @@
+package org.jboss.fuse.largefile;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GenerateCsvFile {
+
+    private static final int ITERATIONS = 5;
+    private static final double MEG = (Math.pow(1024, 2));
+    private static final int RECORD_COUNT = 10000;
+    private static final String HEADERS = "id,first_name,last_name,email,gender,ip_address";
+    private static Random r = new Random();
+    private static final String domain ="@acme.com";
+
+    private static String[] FirstName = { "Charles","James","Claus","Jeff","Rachel","Sylvie","Pauline","Marc","Eric","John","Keith","Ken","Rob" };
+    private static String[] LastName = { "Moulliard","Cassidy","Yordan","Bailly","Baboo","Johnson","Britton","Ibsen","Davies","Strachan","Rawling" };
+
+    public static void main(String[] args) throws Exception {
+        List<String> records = new ArrayList<String>(RECORD_COUNT);
+        StringBuffer buffer;
+
+        int size = 0;
+        for (int i = 0; i < RECORD_COUNT; i++) {
+            buffer = new StringBuffer();
+            String name = generateName();
+            buffer.append(i).append(",").append(name).append(",").append(getEmail(name)).append(",").append(getIpAddress()).append("\n");
+            String record = buffer.toString();
+            records.add(record);
+            size += record.getBytes().length;
+        }
+        System.out.println(records.size() + " 'records'");
+        System.out.println(size / MEG + " MB");
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            System.out.println("\nIteration " + i);
+
+            writeRaw(records);
+            //writeBuffered(records, 8192);
+            //writeBuffered(records, (int) MEG);
+            //writeBuffered(records, 4 * (int) MEG);
+        }
+    }
+
+    private static void writeRaw(List<String> records) throws IOException {
+        String dir = "/Users/chmoulli/RedHat/GPE/GPE-Courses/fuse-integration-advanced/code/large-file-processing/target/data";
+        boolean isCreated = new File(dir).mkdirs();
+
+        File file = File.createTempFile("foo",".txt",new File(dir));
+        try {
+            FileWriter writer = new FileWriter(file);
+            System.out.print("Writing raw... ");
+            write(records, writer);
+        } finally {
+            // comment this out if you want to inspect the files afterward
+            //file.delete();
+        }
+    }
+
+    private static void writeBuffered(List<String> records, int bufSize) throws IOException {
+        File file = File.createTempFile("foo", ".txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer, bufSize);
+
+            System.out.print("Writing buffered (buffer size: " + bufSize + ")... ");
+            write(records, bufferedWriter);
+        } finally {
+            // comment this out if you want to inspect the files afterward
+            file.delete();
+        }
+    }
+
+    private static void write(List<String> records, Writer writer) throws IOException {
+        long start = System.currentTimeMillis();
+        for (String record: records) {
+            writer.write(record);
+        }
+        writer.flush();
+        writer.close();
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) / 1000f + " seconds");
+    }
+
+    public static String generateName() {
+        return FirstName[r.nextInt(FirstName.length)] + "," +
+                LastName[r.nextInt(LastName.length)];
+    }
+
+    public static String getEmail(String name) {
+        String[] parts = name.split(",");
+        StringBuffer buffer = new StringBuffer();
+        return buffer.append(parts[0].toLowerCase()).append(".").append(parts[1].toLowerCase()).append(domain).toString();
+    }
+
+    private static String getIpAddress() {
+        return r.nextInt(256) + "." + r.nextInt(256) + "." + r.nextInt(256) + "." + r.nextInt(256);
+    }
+}
