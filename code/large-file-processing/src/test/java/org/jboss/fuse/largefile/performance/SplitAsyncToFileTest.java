17eefd7eae0b72bd97158c5db6c5723283f33b53
opentlc-user <opentlc-user@forge.opentlc.com>
2016-07-20 10:02:40 -0400

    code skeleton from jenkins

---
+++ code/large-file-processing/src/test/java/org/jboss/fuse/largefile/performance/SplitAsyncToFileTest.java
@@ -0,0 +1,46 @@
+package org.jboss.fuse.largefile.performance;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.util.StopWatch;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class SplitAsyncToFileTest extends CamelTestSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CamelTestSupport.class);
+
+    private int files = 10;
+    private int rows = 50000;
+    private int total = (files * rows) + files;
+
+    @Override
+    public void setUp() throws Exception {
+        deleteDirectory("target/data/out");
+        super.setUp();
+    }
+
+    @Test
+    public void testStream() throws Exception {
+    }
+
+    @Test
+    public void testStreamAndSeda() throws Exception {
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+    }
+
+    private class MyAggregationStrategy implements AggregationStrategy {
+    }
+
+}
+
