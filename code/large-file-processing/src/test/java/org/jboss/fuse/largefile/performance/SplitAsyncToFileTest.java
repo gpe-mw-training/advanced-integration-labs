1f148899335ba44b088bf731b7e37139ba2abd33
opentlc-user <opentlc-user@forge.opentlc.com>
2016-07-20 10:37:40 -0400

    code skeleton from jenkins

--- code/large-file-processing/src/test/java/org/jboss/fuse/largefile/performance/SplitAsyncToFileTest.java
+++ code/large-file-processing/src/test/java/org/jboss/fuse/largefile/performance/SplitAsyncToFileTest.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class SplitAsyncToFileTest extends CamelTestSupport {
